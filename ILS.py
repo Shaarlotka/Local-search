e2b7bb75ce4c06211e4365b26627c6ebb9e1106a
Shaarlotka <tytylik14@gmail.com>
2021-01-23 20:27:47 +0300

    ILS added

--- ILS.py
+++ ILS.py
@@ -1,4 +1,5 @@
 import math
+from random import randint
 
 def greedy_algorithm_tsp(pure_ids, distances):
     temp = dict(item for item in distances.items())
@@ -24,42 +25,44 @@ def greedy_algorithm_tsp(pure_ids, distances):
             j += 1
         temp_list.remove(i)
         solution.append(i)
-    solution.append(pure_ids[0])
     return solution
 
-def calculate_route(solution, distances):
-    print(solution)
-    sum = 0
-    for i in range(len(solution) - 1):
-        if not (solution[i], solution[i + 1]) in distances:
-            sum += distances[solution[i + 1], solution[i]]
-        else:
-            sum += distances[solution[i], solution[i + 1]]
-    print(sum)
-    return sum
 
-def swap_2_opt(current_solution, i, k):
-    new_solution = list()
-    new_solution.append(current_solution[:i])
-    for j in reversed(range(i, k + 1)):
-        new_solution.append(current_solution[j])
-    new_solution.append(current_solution[k + 1:])
-    return new_solution
+def calculate_route_length(solution, distances):
+    length, i = 0, 0
+    for i in range(len(solution) - 1):
+        if (solution[i], solution[i + 1]) not in distances:
+            length += distances[solution[i + 1], solution[i]]
+        else:
+            length += distances[solution[i], solution[i + 1]]
+    length += distances[solution[0], solution[i + 1]]
+    return length
+
 
 def two_opt_swap(solution, i, j):
-    solution[i], solution[j] = solution[j], solution[i]
-    return solution
+    return solution[:i+1] + solution[j-1:i:-1] + solution[j:]
 
-def two_opt(solution, distances):
-    route = calculate_route(solution, distances)
-    for i in range(1, len(solution) - 2):
-        for j in range(i + 1, len(solution) - 1):
+
+
+def perturbation(solution):
+    new_solution = solution
+    i, j = 0, 0
+    while (i == j):
+        i = randint(1, len(solution) - 1)
+        j = randint(1, len(solution) - 1)
+    new_solution[i], new_solution[j] = new_solution[j], new_solution[i]
+    return new_solution
+
+
+def two_opt(solution, distances, route_length):
+    for i in range(1, len(solution) - 1):
+        for j in range(i + 1, len(solution)):
             new_solution = two_opt_swap(solution, i, j)
-            new_route = calculate_route(new_solution, distances)
-            if (new_route < route):
+            new_route_length = calculate_route_length(new_solution, distances)
+            if (new_route_length < route_length):
                 solution = new_solution
-                route = new_route
-    return solution            
+                route_length = new_route_length
+    return solution        
 
 
 def euclid_cout_distace(vert):
@@ -67,14 +70,32 @@ def euclid_cout_distace(vert):
     distances = dict()
     for id_1 in ids:
         for id_2 in ids:
-            if not (id_2, id_1) in distances and id_1 != id_2:
+            if (id_2, id_1) not in distances and id_1 != id_2:
                 distances[id_1, id_2] = math.sqrt((vert[id_1][0] - vert[id_2][0])**2 +
                                                   (vert[id_1][1] - vert[id_2][1])**2)
     return ids, distances
 
 
+def ILS(ids, distances):
+    initial_solution = greedy_algorithm_tsp(ids, distances)
+    solution = two_opt(initial_solution,distances,
+                       calculate_route_length(initial_solution, distances))
+    route_length = calculate_route_length(solution, distances)
+    for i in solution:
+        tmp_sol = [items for items in solution]
+        new_solution = perturbation(tmp_sol)
+        new_solution = two_opt(new_solution, distances,
+                               calculate_route_length(new_solution, distances))
+        new_route_length = calculate_route_length(new_solution, distances)
+        if (new_route_length < route_length):
+            route_length = new_route_length
+            solution = [items for items in new_solution]
+    return solution
+
+
 if __name__ == "__main__":
-    vert = {0: [2, 5], 1: [23, 3], 2: [3, 8], 3: [4, 15], 4: [35, 13], 5: [23,54], 6: [54,6], 7: [5,8]}
+    vert = {0: [2, 5], 1: [23, 3], 2: [3, 8], 3: [4, 15], 4: [35, 13], 5: [23,54], 6: [54,6], 7: [5,8],
+            8: [2, 5], 9: [1, 7], 10: [12, 3], 11: [9, 5]}
     ids, distances = euclid_cout_distace(vert)
-    solution = greedy_algorithm_tsp(ids, distances)
-    new_solution = two_opt(solution, distances)
+    tmp = ILS(ids, distances)
+    print(tmp, calculate_route_length(tmp, distances))
